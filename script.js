e2b6b6389a0e7a9bab4681c1adcb5e41d63c08ba
Jaikrishna Binnar <86294906+jaikrushna@users.noreply.github.com>
2026-04-22 17:28:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
 //complete this code
-class Person {}
+class Person {
+	var name;
+	var age;
+	constructor(name, age)
+	{
+		this.name = name;
+		this.age = age;
+	}
+	set set_age(age)
+	{
+		this.age = age;
+	}
+	get get_name 
+	{
+		return name;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	constructor(name, age)
+	{
+		super(name, age);
+	}
+	void study()
+	{
+		console.log(`${name} is studying`);
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	constructor(name, age)
+	{
+		super(name, age);
+	}
+	void teach()
+	{
+		console.log(`${name} is teaching`);
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
